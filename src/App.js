cb51c770de60ca99fb807f4ff4a819f9b3baa248
Duracyq <szymon.kojek@wp.pl>
2023-05-24 20:49:04 +0200

    tried to add functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [statements, setStatements] = useState([])
+
+  const loadStatements = async () => {
+    try {
+      const response = await fetch('./postulaty.json');
+      const data = await response.json();
+      setStatements(data);
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>My Portfolio</h1>
+      {/* <h1>My Portfolio</h1>
 
-      <div class="project">
+      <div className="project">
         <h2>Project 1</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et mi consequat, vestibulum nunc vel, cursus justo.</p>
       </div>
 
-      <div class="project">
+      <div className="project">
         <h2>Project 2</h2>
         <p>Donec ac urna condimentum, hendrerit enim vitae, faucibus risus. Sed tempus libero a neque placerat dignissim.</p>
       </div>
 
-      <div class="project">
+      <div className="project">
         <h2>Project 3</h2>
         <p>Integer eleifend dolor sit amet ex tristique, in lobortis ipsum pulvinar. Vestibulum auctor leo a tellus convallis malesuada.</p>
-      </div>
+      </div> */}
+      <button onClick={loadStatements}>ASDASDA</button>
     </div>
   );
 }
